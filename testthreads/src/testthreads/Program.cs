863b93a7bcc32412183a40e0237dabf05ae3426c
savachebra <savachebra@gmail.com>
2019-01-05 16:55:24 +0200

    Mutexing

--- testthreads/src/testthreads/Program.cs
+++ testthreads/src/testthreads/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace testthreads
 {
     public class Program
     {
+        static readonly Mutex mutex = new Mutex(false, "Mutex_Test");
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            for (int i = 0; i < 10; i++)
+            {
+                var thread = new Thread(AquireMutex);
+                thread.Start($"Thread {i}");
+            }        
+        }
+
+        private static void AquireMutex(object threadName)
+        {
+            mutex.WaitOne();
+            DoSomething();
+            Console.WriteLine($"Mutex aquired by {threadName}");
+            mutex.ReleaseMutex();
+            Console.WriteLine($"Mutex released by {threadName}");
+        }
+
+        private static void DoSomething()
+        {
+            Thread.Sleep(50);
         }
     }
 }
