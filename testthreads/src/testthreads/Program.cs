42b2a2348bf12968782e264e6fb858b8df0342a4
savachebra <savachebra@gmail.com>
2019-01-05 14:39:58 +0200

    new branch commit

--- testthreads/src/testthreads/Program.cs
+++ testthreads/src/testthreads/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace testthreads
 {
@@ -9,6 +6,7 @@ namespace testthreads
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
         }
     }
 }
